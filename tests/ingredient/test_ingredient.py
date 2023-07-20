29abc266eb918817333fdaef1f5c95911e4cddce
yurioneix <yurioneix18@gmail.com>
2023-07-20 16:10:29 -0300

    Requisito 1: implementa testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("farinha")
+
+    assert ingredient_1.name == "farinha"
+    assert ingredient_1.restrictions == {Restriction.GLUTEN}
+
+    ingredient_2 = Ingredient("ovo")
+    assert not ingredient_1 == ingredient_2
+    assert repr(ingredient_2) == "Ingredient('ovo')"
+
+    ingredient_3 = Ingredient("farinha")
+    assert ingredient_1.__hash__() == ingredient_3.__hash__()
+    assert ingredient_2.__hash__() != ingredient_1.__hash__()
+    assert ingredient_3 == ingredient_1
